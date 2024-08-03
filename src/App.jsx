4f1ce6d4d6ebdc2f68f6089bbbc7f17c736b070c
thesegunonakoya <thesegunonakoya@gmail.com>
2024-08-03 19:01:56 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import PlayersList from './components/PlayersList';
 
 function App() {
   return (
+    
     <div className="App">
       <h1>FIFA Player Cards</h1>
       <PlayersList />
     </div>
+
   );
 }
 
